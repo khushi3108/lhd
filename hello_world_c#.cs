449ecb4f72fe52edb8730f7006754994aa8dc3f9
Khushi Gandhi <60807052+khushi3108@users.noreply.github.com>
2021-01-11 21:50:11 +0530

    Hello World in a new language-C#

---
+++ hello_world_c#.cs
@@ -0,0 +1,21 @@
+// Hello World! program
+
+using System; 
+  
+
+namespace HelloWorldApp { 
+      
+    // Class declaration 
+    class helloworld { 
+          
+        
+        static void Main(string[] args) { 
+              
+        
+            Console.WriteLine("Hello World!"); 
+              
+            
+            
+        } 
+    } 
+} 
